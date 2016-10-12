fdc9856ef815f645be31ec3a1af7ed1cfc537464
feegg333 <riskyliu1220@gmail.com>
2016-10-12 16:31:03 +0800

    update RWD_Ads

---
+++ Scripts/ads.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    $("#btn_1").click(function () {
+        $(".banner").animate({
+            width: "302",
+            height: "252",
+        }, 1500);
+    });
+
+    $("#btn_2").click(function () {
+        $(".banner").animate({
+            width: "162",
+            height: "602",
+        }, 1500);
+    });
+
+    $("#btn_3").click(function () {
+        $(".banner").animate({
+            width: "730",
+            height: "92",
+        }, 1500);
+    });
+
+    $("#btn_4").click(function () {
+        $(".banner").animate({
+            width: "338",
+            height: "282",
+        }, 1500);
+    });
+
+    $("#btn_5").click(function () {
+        $(".banner").animate({
+            width: "302",
+            height: "602",
+        }, 1500);
+    });
+
+}); // jQuery ready-end
